648bca007a1d8198625e513be33a770d0830ac03
Mateo Salazar <msalazar9742@gmail.com>
2020-03-10 23:28:43 -0500

    Added middleware for the errors

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,17 @@ import { apiDoc } from './api/api-doc';
 import cors from 'cors';
 import express from 'express';
 import { initialize } from 'express-openapi';
+import { middlewareErrorHandler } from './utils/errorHandler';
+import multer from 'multer';
+
 const app = express();
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage,
+  limits: {
+    fileSize: 1024 * 1024 * 10, // 10 MB,
+  },
+});
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,6 +23,20 @@ initialize({
   app,
   apiDoc,
   paths: path.join(__dirname, 'api', 'paths'),
+  errorMiddleware: middlewareErrorHandler,
+  consumesMiddleware: {
+    'multipart/form-data': (req: any, res: express.Response, next: express.NextFunction) => {
+      upload.any()(req, res, err => {
+        if (err) { return next(err); }
+        req.files.forEach((f: Express.Multer.File) => {
+          req.body[f.fieldname] = '';
+        });
+
+        return next();
+      });
+    },
+  },
+
 });
 
 // Add a dummy route for root so we pass the health check
